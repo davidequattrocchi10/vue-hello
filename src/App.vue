c341a8dfc7bf0a76addc016bd8fd339c6fd21d27
davidequattrocchi10 <davidequattrocchi10@gmail.com>
2024-01-28 22:41:20 +0100

    print hello Vue with image

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script >
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Hello Vue!",
+      text: "color-text",
+      imageURL: "https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png",
+      image: "image-size"
+    }
+  },
+}
+</script>
+
+<template>
+  <header>
+    <h1 :class="text"> {{ message }}</h1>
+  </header>
+  <main>
+    <img :class="image" :src="imageURL" alt="Vue.js">
+  </main>
+</template>
+
+<style ></style>
